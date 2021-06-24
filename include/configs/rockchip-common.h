34cf6e6caa16985bc72034ae7049c392b75afbe6
hejiawencc <hjw0415@outlook.com>
2021-06-24 15:32:33 +0800

    feat:rk3399 add support rk3399

--- include/configs/rockchip-common.h
+++ include/configs/rockchip-common.h
@@ -144,8 +144,8 @@
 	"boot_android ${devtype} ${devnum};"
 #else
 #define RKIMG_BOOTCOMMAND			\
-	"load mmc 1:1 0x02008000 kernel/vmlinuz-5.10.25-rk3328;"	\
-	"load mmc 1:2 0x08300000 /usr/lib/linux-image-5.10.25-rk3328/rockchip/rk3328-doornet1.dtb;"				\
+	"load mmc 1:1 0x02008000 kernel/vmlinuz-5.10.25-rk3399;"	\
+	"load mmc 1:2 0x08300000 /usr/lib/linux-image-5.10.25-rk3399/rockchip/rk3399-nanopi-r4s.dtb;"				\
 	"setenv bootargs console=ttyS2,1500000 root=/dev/mmcblk0p2 rw rootfstype=ext4 rootwait;"		\
 	"booti 0x02008000 - 0x08300000;"
 #endif
