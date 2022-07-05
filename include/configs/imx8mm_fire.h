58ef4b9ad087d923e5275a2adbe2b84c0bd27682
Louis <1270384833@qq.com>
2022-07-05 17:15:55 +0800

    fixed some issues

--- include/configs/imx8mm_fire.h
+++ include/configs/imx8mm_fire.h
@@ -79,12 +79,12 @@
  */
 #define JAILHOUSE_ENV \
 	"jh_clk= \0 " \
-	"jh_mmcboot=mw 0x303d0518 0xff; setenv fdt_file imx8mm-evk-root.dtb;" \
+	"jh_mmcboot=mw 0x303d0518 0xff; setenv fdt_file imx8mm-fire-root.dtb;" \
 		"setenv jh_clk clk_ignore_unused; " \
 			   "if run loadimage; then " \
 				   "run mmcboot; " \
 			   "else run jh_netboot; fi; \0" \
-	"jh_netboot=mw 0x303d0518 0xff; setenv fdt_file imx8mm-evk-root.dtb; setenv jh_clk clk_ignore_unused; run netboot; \0 "
+	"jh_netboot=mw 0x303d0518 0xff; setenv fdt_file imx8mm-fire-root.dtb; setenv jh_clk clk_ignore_unused; run netboot; \0 "
 
 
 #define CONFIG_MFG_ENV_SETTINGS \
